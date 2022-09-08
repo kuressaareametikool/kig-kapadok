dba4d3e3dd9059bf093a04c0ac04f376e97235aa
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-09 01:12:12 +0300

    pets page, code beautify & fixing errors

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         document.getElementById("splash").style.opacity=0;
-    }, 2000);
+    }, 1000);
 })
