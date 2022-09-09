5d6a68f42d8ef01b6cd9729dedc65452e196c3a4
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-09 12:49:12 +0300

    final!

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         document.getElementById("splash").style.display = "none";
-    }, 2000);
+    }, 3000);
 })
